db1c95214839ef4b5ae64d00931c049122bef2e7
Miisteu <vanhoejasper@gmail.com>
2025-11-28 02:42:15 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,25 @@
+// Alle producten, prijzen en afbeeldingen hier configureren
+const PRODUCTS = [
+  {
+    name: "Verox.GG - Cheat Menu",
+    description: "High quality Cheat Menu.",
+    img: "images/cheat.png",
+    licenses: {
+      "1 Day": 2.50,
+      "3 Days": 6.50,
+      "1 Month": 15,
+      "Lifetime": 30
+    }
+  },
+  {
+    name: "Verox.GG - Spoof Menu",
+    description: "High quality Spoof Menu.",
+    img: "images/spoof.png",
+    licenses: {
+      "1 Day": 1,
+      "3 Days": 4,
+      "1 Month": 12,
+      "Lifetime": 25
+    }
+  }
+];
